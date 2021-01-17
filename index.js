28408fcdac5ac5dd9ff2d3228257f7750e8f10a3
dave ready <davis_ready@hotmail.com>
2021-01-17 13:31:44 -0500

    plugged in user questions/prompts into template

--- index.js
+++ index.js
@@ -2,54 +2,55 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-//use inquirer.prompt to create questions for user
+//use inquirer.prompt to create questions/prompts for user
 
 inquirer
   .prompt([
       {
-        type:'',
-        message:'',
-        name:'',
+        type:'input',
+        message:"Enter your project's title",
+        name:'Title',
       },
       {
-        type:'',
-        message:'',
-        name:'',
+        type:'input',
+        message:"Enter a short description of your project",
+        name:'Description',
     },
     {
-        type:'',
-        message:'',
-        name:'',
+        type:'input',
+        message:"Enter the installation process",
+        name:'Installation',
     },
     {
-        type:'',
-        message:'',
-        name:'',
+        type:'input',
+        message:"Enter the usage of the project",
+        name:'Usage',
     },
     {
-        type:'',
-        message:'',
-        name:'',
+        type:'list',
+        message:"Select licences used for your project",
+        name:'Lincenses',
+        choices: 'Apache 2.0','Boost 1.0', 'MIT', 'GNU 2.0', 'GNU 3.0',
     },
     {
-        type:'',
-        message:'',
-        name:'',
+        type:'input',
+        message:"Enter any contributing info",
+        name:'Contributing'
     },
     {
-        type:'',
-        message:'',
-        name:'',
+        type:'input',
+        message:"Enter any tests you are running for your project",
+        name:'Tests',
     },
     {
-        type:'',
-        message:'',
-        name:'',
+        type:'input',
+        message:"Enter your GitHub username",
+        name:'Questions',
     },
     {
-        type:'',
-        message:'',
-        name:'',
+        type:'input',
+        message:"Enter your email address",
+        name:'Questions',
     },
 
   ])
