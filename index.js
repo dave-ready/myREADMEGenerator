0d7b8ec8f1094062ff3f9f8d5991a6c93ca93f10
dave ready <davis_ready@hotmail.com>
2021-01-17 12:55:35 -0500

    created question template

---
+++ index.js
@@ -0,0 +1,55 @@
+//'require' node modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//use inquirer.prompt to create questions for user
+
+inquirer
+  .prompt([
+      {
+        type:'',
+        message:'',
+        name:'',
+      },
+      {
+        type:'',
+        message:'',
+        name:'',
+    },
+    {
+        type:'',
+        message:'',
+        name:'',
+    },
+    {
+        type:'',
+        message:'',
+        name:'',
+    },
+    {
+        type:'',
+        message:'',
+        name:'',
+    },
+    {
+        type:'',
+        message:'',
+        name:'',
+    },
+    {
+        type:'',
+        message:'',
+        name:'',
+    },
+    {
+        type:'',
+        message:'',
+        name:'',
+    },
+    {
+        type:'',
+        message:'',
+        name:'',
+    },
+
+  ])
