fb5db82ec038a2a47cce5cf7b03480e8de38b664
dave ready <davis_ready@hotmail.com>
2021-01-19 17:41:52 -0500

    wrapped inquirer.prompt in promptUser function + util+writeFileAsync consts

--- index.js
+++ index.js
@@ -2,65 +2,66 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-//use inquirer.prompt to create questions/prompts for user
+//use inquirer. to create questions/prompts for user
 
 inquirer
   .prompt([
       {
         type:'input',
         message:"Enter your project's title",
-        name:'Title',
+        name:'title',
       },
       {
         type:'input',
         message:"Enter a short description of your project",
-        name:'Description',
+        name:'description',
     },
     {
         type:'input',
         message:"Enter the installation process",
-        name:'Installation',
+        name:'installation',
     },
     {
         type:'input',
         message:"Enter the usage of the project",
-        name:'Usage',
+        name:'usage',
     },
     {
         type:'list',
         message:"Select licences used for your project",
-        name:'Licenses',
-        choices: ['Apache 2.0','Boost 1.0', 'MIT', 'GNU 2.0', 'GNU 3.0']
+        name:'licenses',
+        choices: ['Apache 2.0','Boost 1.0', 'MIT', 'BSD 2.0', 'BSD 3.0']
     },
     {
         type:'input',
         message:"Enter any contributing info",
-        name:'Contributing'
+        name:'contributing'
     },
     {
         type:'input',
         message:"Enter any tests you are running for your project",
-        name:'Tests',
+        name:'tests',
     },
     {
         type:'input',
         message:"Enter your GitHub username",
-        name:'Username',
+        name:'username',
     },
     {
         type:'input',
         message:"Enter your email address",
-        name:'Email',
+        name:'email',
     },
 
-  ]);
+  ]).then(({title, description, installation, usage, licenses, contributing, tests, username,email}) => {
 
 
-//Create README Template/Save as a const
+//Create README Template as a const
   const readMeTemplate = `
 
-  ##${response.licenses}
-  #${response.title}
+  ##${licenses}
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]
+  #${title}
 
 
   ##Table of Contents
@@ -72,19 +73,32 @@ inquirer
 
   
   ##Description
-  ${response.description}
+  ${description}
   
   ##Installation
-  ${response.installation}
+  ${installation}
 
   ##Usage
-  ${response.usage}
+  ${usage}
 
   ##Contributing
-  ${response.contributing}
+  ${contributing}
 
   ##Contact
-  -Github Username: ${response.username}
-  -E-mail: ${response.email}
-  
+  -Github Username: ${username}
+  -E-mail: ${email}
   `;
+  generateNewFile(title, readMeTemplate)
+});
+
+//Create a function to generate readme using file system
+
+function generateNewFile(fileName, data) {
+    fs.writefile(`./${fileName.toLowerCase().split(' ').join('')}.md`,data,(err) => {
+        if(err) {
+            console.log(err)
+        }
+        console.log("Your README is now complete!!");
+
+    })
+}
